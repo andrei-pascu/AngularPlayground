df0e631bd1a510453ef7b5d25eaf1de016c0fd33
Pascu Andrei <30505787+andrei-pascu@users.noreply.github.com>
2019-03-27 12:32:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+var app = angular.module('myApp', []);
+
+app.controller('myController', function($scope) {
+    $scope.checklength = null;
+    // $scope.email = 'yyy@uu.ll';
+        // $scope.formData = {
+        //     "Name": 'sdad', 
+        //     "Email": 'asaaa', 
+        //     "At": 'asasdsdaa', 
+        //     "LengthCheck": 'asasdadaaa'
+           
+        // };
+
+    $scope.getFormState = function() {
+            var validN = ($scope.aForm.name.$valid && $scope.aForm.name.$dirty) ? 1 : 0;
+            var validE = $scope.aForm.email.$valid ? 1 : 0;
+            var validM = $scope.aForm.myInput.$valid ? 1 : 0;
+            var validP = $scope.aForm.checklength.$valid ? 1 : 0;
+    
+            console.log(validN)
+            console.log(validE)
+            console.log(validM)
+            console.log(validP)
+            console.log("Form state " + (validN + validE + validM + validP))
+
+            var formValidStates = -100 + (validN + validE + validM + validP) * 25;
+            $scope.formCompletion = {
+                "transform" : 'translateY('+formValidStates+'%)'
+            }
+    }
+
+    $scope.catchLength = function($event) {
+        console.log($scope.aForm.checklength.$modelValue)
+
+        if ($scope.checklength == undefined) {
+            $scope.checklengthDigitsLength = 0;
+        } else if ($scope.checklength.length > 10 ) {
+            $scope.aForm.checklength.$setValidity('x', false)
+            $scope.checklengthDigitsLengthMax = "#881414";
+        } else if ($scope.checklength.length == 10 ) {
+            $scope.checklengthDigitsLengthMax = "#b58707";
+            $scope.aForm.checklength.$setValidity('x', true)
+        } else {
+            $scope.aForm.checklength.$setValidity('x', false)
+            $scope.checklengthDigitsLengthMax = "#3d4b8c";
+            $scope.checklengthDigitsLength = ($scope.checklength.length) * 10;
+        }
+        $scope.checklengthLength = {
+            "width" : $scope.checklengthDigitsLength + "%",
+            "background-color" : $scope.checklengthDigitsLengthMax
+        }
+        var validN = ($scope.aForm.name.$valid && $scope.aForm.name.$dirty) ? 1 : 0;
+        var validE = $scope.aForm.email.$valid ? 1 : 0;
+        var validM = $scope.aForm.myInput.$valid ? 1 : 0;
+        var validP = $scope.aForm.checklength.$valid ? 1 : 0;
+    }
+
+    $scope.submitMockup = function() {
+        $scope.formData = {
+            "Name": $scope.aForm.name.$modelValue, 
+            "Email": $scope.aForm.email.$modelValue, 
+            "At": $scope.aForm.myInput.$modelValue, 
+            "LengthCheck": $scope.aForm.checklength.$modelValue
+        };
+    }
+    $scope.AsyncData
+    
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+            $scope.AsyncData = json.title;
+            document.getElementById('async').innerText = $scope.AsyncData.toUpperCase();
+            console.log($scope.AsyncData)
+        })
+});
+
+
+app.directive('myDirective', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attr, mCtrl) {
+            function myValidation(value) {
+                if (value == undefined) {
+                    mCtrl.$setValidity('charA', false);
+                } else if (value.indexOf("@") > -1) {
+                    mCtrl.$setValidity('charA', true);
+                } else {
+                    mCtrl.$setValidity('charA', false);
+                }
+                return value;
+            }
+            mCtrl.$parsers.push(myValidation);
+        }
+    };
+});
+
+
