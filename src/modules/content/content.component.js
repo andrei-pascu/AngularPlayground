51abe3fe7b35599c6f1e2c1cf93b62511edfab2b
PascuAndreiSoftvision <andrei.pascu@softvision.ro>
2019-03-29 13:17:52 +0200

    Typo

--- src/modules/content/content.component.js
+++ src/modules/content/content.component.js
@@ -1,18 +1,12 @@
+import {template} from './content.template';
 import {formTemplate} from './form-template/form-template.component';
 
 function appMainComponent() {
     var app = angular.module('myApp');
 
     formTemplate()
-    let appContentTemplate = `    
-        <div class="content">
-        <h1 class="page-title">Forms Playground V2</h1>
-            <my-custom-form class="form-container"></my-custom-form>
-        </div>
-        </div>
-        `
     app.component('appMainComponent', {
-    template: appContentTemplate
+    template: template
     })
 }
 
--- src/modules/content/content.template.js
+++ src/modules/content/content.template.js
@@ -1,4 +1,4 @@
-export const appContentTemplate = `    
+export const template = `    
     <div class="content">
     <h1 class="page-title">Forms Playground V2</h1>
         <my-custom-form class="form-container"></my-custom-form>
